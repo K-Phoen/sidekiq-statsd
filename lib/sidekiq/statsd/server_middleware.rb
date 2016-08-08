3d39e3e9fec0d22b93177c517fcf38e838d821ce
Kévin Gomez <kgomez@tea-ebook.com>
2016-08-08 17:33:30 +0200

    Send workers metrics as batches

--- lib/sidekiq/statsd/server_middleware.rb
+++ lib/sidekiq/statsd/server_middleware.rb
@@ -13,30 +13,32 @@ module Sidekiq::Statsd
     def call(worker, msg, queue)
       worker_name = worker.class.name.gsub('::', '.')
 
-      begin
-        @statsd.time prefix(worker_name, 'processing_time') do
-          yield
-        end
+      @statsd.batch do |b|
+        begin
+          b.time prefix(worker_name, 'processing_time') do
+            yield
+          end
 
-        @statsd.increment prefix(worker_name, 'success')
-      rescue => e
-        @statsd.increment prefix(worker_name, 'failure')
-        raise e
-      ensure
-        send_global_stats if @options[:sidekiq_stats]
+          b.increment prefix(worker_name, 'success')
+        rescue => e
+          b.increment prefix(worker_name, 'failure')
+          raise e
+        ensure
+          send_global_stats(b) if @options[:sidekiq_stats]
+        end
       end
     end
 
     private
 
-    def send_global_stats
+    def send_global_stats(statsd)
       # Queue sizes
-      @statsd.gauge(prefix('enqueued'), @sidekiq_stats.enqueued)
-      @statsd.gauge(prefix('retry_set_size'), @sidekiq_stats.retry_size)
+      statsd.gauge(prefix('enqueued'), @sidekiq_stats.enqueued)
+      statsd.gauge(prefix('retry_set_size'), @sidekiq_stats.retry_size)
 
       # All-time counts
-      @statsd.gauge(prefix('processed'), @sidekiq_stats.processed)
-      @statsd.gauge(prefix('failed'), @sidekiq_stats.failed)
+      statsd.gauge(prefix('processed'), @sidekiq_stats.processed)
+      statsd.gauge(prefix('failed'), @sidekiq_stats.failed)
     end
 
     def prefix(*args)
